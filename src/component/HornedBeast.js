7f64e2254ef344806a028a620fec022c0c436378
anas <civil.anas1000@gmail.com>
2021-06-07 14:18:21 +0300

    finish lab 02

--- src/component/HornedBeast.js
+++ src/component/HornedBeast.js
@@ -3,12 +3,26 @@ import React from 'react';
 
 class HornedBeast extends React.Component{
 
+    constructor(props){
+        super(props)
+        this.state={
+            numlikes:0
+        }
+    }
+
+    incrementNumberOFlikes=()=>{
+        this.setState=({
+            numlikes:this.state.numlikes+1
+        })
+    }
+
     render(){
         return(
             <div className='card'>
                 <h2> {this.props.title} </h2>
-                <img alt={this.props.alt}  src={this.props.url} />
+                <img onClick={this.incrementNumberOFlikes} alt={this.props.alt}  src={this.props.url} />
                 <p> {this.props.description} </p>
+                <p> Number of likes {this.state.numlikes} </p>
             </div>
         );
     }
--- src/component/Main.js
+++ src/component/Main.js
@@ -1,22 +1,29 @@
 
 import React from 'react';
-import HornedBeast from'./HornedBeast';
+import HornedBeast from './HornedBeast';
+import rawData from './data.json'
 
 class Main extends React.Component {
+
     render() {
+
         return (
+
             <div className='main' >
+                {rawData.map((item) => {
+                    return (
+                        <div>
+                            <HornedBeast alt={item.keyword} description={item.description} title={item.description} url={item.image_url} />
 
-                <h2> HornedBeast  </h2>
-                
-                <HornedBeast alt={'narwhal'} description={"A unicorn and a narwhal nuzzling their horns"} title={'UniWhal'}  url={'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg'} />
+                        </div>
 
-                <HornedBeast alt={'rhino'} description={" Mother (or father) rhino with two babies "} title={'Rhino Family'} url={'https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80'}/>
+                    )
 
-                <HornedBeast alt={'unicorn'} description= {" Someone wearing a creepy unicorn head mask" }title={'Unicorn Head'} url={'https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg'} />
+                })}
 
             </div>
         )
+
     }
 }
 
