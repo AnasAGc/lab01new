17f7be7f8611c599f88087719c67d94869e540b7
anas <civil.anas1000@gmail.com>
2021-06-07 14:53:51 +0300

    adding boot strap

--- src/component/Main.js
+++ src/component/Main.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
 import rawData from './data.json'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
 class Main extends React.Component {
 
@@ -10,18 +12,29 @@ class Main extends React.Component {
         return (
 
             <div className='main' >
-                {rawData.map((item,index) => {
-                                                console.log(index);
+                {/* {rawData.map((item, index) => {
 
                     return (
-                        <div className={'card}'+index} >
-                            <HornedBeast  alt={item.keyword} description={item.description} title={item.title} url={item.image_url} />
+                        <div className={'card}' + index} >
+                            <HornedBeast alt={item.keyword} description={item.description} title={item.title} url={item.image_url} />
 
                         </div>
 
                     )
 
-                })}
+                })} */}
+
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+    </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
 
             </div>
         )
